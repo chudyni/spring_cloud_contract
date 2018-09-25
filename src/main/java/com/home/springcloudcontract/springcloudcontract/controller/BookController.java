7e217ef3caa8df6a9538bd75b2f0376c2f6b41cc
Marcin Bracisiewicz <m.bracisiewicz@gmail.com>
2018-09-25 21:43:43 +0200

    unresolved problems with dependencies

--- src/main/java/com/home/springcloudcontract/springcloudcontract/controller/BookController.java
+++ src/main/java/com/home/springcloudcontract/springcloudcontract/controller/BookController.java
@@ -1,11 +1,15 @@
 package com.home.springcloudcontract.springcloudcontract.controller;
 
+
 import com.home.springcloudcontract.springcloudcontract.model.Book;
 import com.home.springcloudcontract.springcloudcontract.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by marcin.bracisiewicz
@@ -19,10 +23,9 @@ public class BookController {
 
     @RequestMapping(
             method = RequestMethod.POST,
-            produces = MediaType.APPLICATION_JSON_VALUE
-    )
+            produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseStatus(HttpStatus.CREATED)
     public Book createBook(@RequestBody Book book) {
-        return this.bookService.createNew(book);
+        return bookService.createNew(book);
     }
 }
